5a2855dbe59b79162b76925fe295cb97607af3fc
alaa ahmad altahan <alaath777@gmail.com>
2024-12-31 13:56:24 +0300

    add function operator

--- scrpt.js
+++ scrpt.js
@@ -14,3 +14,22 @@ function divide(a, b){
     return a / b
 }
 
+function operate(a, operator , b){
+    switch (operator){
+        case "+" :
+        add(a, b);
+        break;
+        case "-" :
+        minus(a, b);
+        break;
+        case "*" :
+        times(a, b);
+        break;
+        case "/" :
+        divide(a, b);
+        break;
+
+        default:
+            alert("please enter oneof the following operator: +, -, *, /")
+    }
+}
