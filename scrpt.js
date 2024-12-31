0089a0a75deef7eb85741e671a450286f27418af
alaa ahmad altahan <alaath777@gmail.com>
2024-12-31 13:33:32 +0300

    add functions (add,subtract,times,divide)

---
+++ scrpt.js
@@ -0,0 +1,16 @@
+function add(a , b){
+    return a  + b
+} 
+
+function minus(a, b){
+    return a - b
+}
+
+function times(a, b){
+    return a * b
+}
+
+function divide(a, b){
+    return a / b
+}
+
